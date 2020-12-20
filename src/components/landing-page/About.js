916cd8fddf351ed6e6ac188122bdfa92a216077f
Rachel Reilly <r.a.reill18@gmail.com>
2020-12-20 14:08:14 -0600

    learn more going to about page with content

---
+++ src/components/landing-page/About.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { IoMdArrowBack } from 'react-icons/io';
+import { SiGithub, SiLinkedin } from 'react-icons/si';
+import '../../css/About.css';
+
+function About(props) {
+  return (
+    <div className='about-wrapper'>
+      <div className='about-header-wrapper'>
+        <IoMdArrowBack
+          tabIndex='0'
+          className='back-button'
+          onClick={() => props.hideAbout()}
+          onKeyPress={e => e.key === 'Enter' ? props.hideAbout() : null} />
+        <h2>One Dimensional Chess</h2>
+      </div>
+
+
+      <div className='about-content-wrapper'>
+        <h3>About</h3>
+        <p>There are many versions of one-dimensional chess. This game uses Glimne's rules, adapted from <a href='https://www.chessvariants.com/shape.dir/onedim.html'>The Chess Variants</a>.</p>
+        <p>Glimne's version has 18 squares. Each side has 7 pieces starting in the following order: King, Queen, Rook, Bishop, Bishop, Knight, Pawn.</p>
+        <p>Instructions for how each piece can moved are displayed at the top of the game. </p>
+        <p>This game was created by <a href='https://rachelanastasia.netlify.app/'>Rachel Reilly</a>, a Chicago based JavaScript developer, using React and CSS3.</p>
+        <div className='about-icon-container'>
+          <a href='https://www.linkedin.com/in/rachel-a-reilly/'>
+            <SiLinkedin className='about-icon' />
+          </a>
+          <a href='https://github.com/Rachanastasia'>
+            <SiGithub className='about-icon' />
+          </a>
+
+        </div>
+
+        <p></p>
+      </div>
+    </div>
+
+  )
+}
+
+export default About
--- src/components/landing-page/LandingPage.js
+++ src/components/landing-page/LandingPage.js
@@ -1,21 +1,31 @@
-import React, { useContext } from 'react';
+import React, { useContext, Fragment, useEffect, useState } from 'react';
 import Header from './Header';
 import JumpingPawn from './JumpingPawn';
 import { GameContext } from '../../contexts/GameContext';
+import About from './About';
 
 function LandingPage(props) {
   const { setPlaying } = useContext(GameContext);
+  const [showAbout, setShowAbout] = useState(false);
+
+  //resets showAbout after render
+  useEffect(() => {
+    return () => setShowAbout(false);
+  }, [])
+
   return (
-    <div className='landing-page-wrapper'>
-
-      <Header />
-      <JumpingPawn />
-      <div className='play-button-wrapper'>
-        <button onClick={() => setPlaying(true)}>Play</button>
-        <button>Learn more</button>
-
-      </div>
-    </div>
+    <Fragment>
+      {!showAbout
+        ? <div className='landing-page-wrapper'>
+          <Header />
+          <JumpingPawn />
+          <div className='play-button-wrapper'>
+            <button onClick={() => setPlaying(true)}>Play</button>
+            <button onClick={() => setShowAbout(true)}>Learn more</button>
+          </div>
+        </div>
+        : <About hideAbout={() => setShowAbout(false)} />}
+    </Fragment>
   )
 }
 
